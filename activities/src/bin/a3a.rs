12135dd13365453621d74bf832e4b85dd864e414
Santiago Arboleda Londoño <monoprosito@gmail.com>
2021-11-04 21:25:32 -0500

    Print a salute according a boolean variable

--- activities/src/bin/a3a.rs
+++ activities/src/bin/a3a.rs
@@ -10,4 +10,19 @@
 // * Use an if..else block to determine which message to display
 // * Use the println macro to display messages to the terminal
 
-fn main() {}
+
+// * Displays a message based on the value of a boolean variable
+fn display(salute: bool) {
+    if salute {
+        // When the variable is set to true, display "hello"
+        println!("hello");
+    } else {
+        // When the variable is set to false, display "goodbye"
+        println!("goodbye");
+    }
+}
+
+fn main() {
+    let salute: bool = false;
+    display(salute);
+}
