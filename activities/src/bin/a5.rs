380a0b32011f3da00eb15869dc612904e9d5ed93
Santiago Arboleda Londoño <monoprosito@gmail.com>
2021-11-12 18:59:39 -0500

    Print numbers in a loop

--- activities/src/bin/a5.rs
+++ activities/src/bin/a5.rs
@@ -9,4 +9,19 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+fn main() {
+    // * Use a mutable integer variable
+    let mut n = 1;
+    let limit = 4;
+
+    // * Use a loop statement
+    loop {
+        // * Print the variable within the loop statement
+        println!("{:?}", n);
+        if n == limit {
+            // * Use break to exit the loop
+            break;
+        }
+        n += 1;
+    }
+}
