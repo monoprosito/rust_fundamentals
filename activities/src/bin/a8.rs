fa84043bd5797a4a52c003c1fc263e910a97f572
Santiago Arboleda Londoño <monoprosito@gmail.com>
2021-11-12 19:51:31 -0500

    Print the properties of two drinks

--- activities/src/bin/a8.rs
+++ activities/src/bin/a8.rs
@@ -9,4 +9,41 @@
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
 
-fn main() {}
+
+// * Use an enum to create different flavors of drinks
+enum Flavor {
+    CocaCola,
+    Pepsi
+}
+
+// * Use a struct to store drink flavor and fluid ounce information
+struct Drink {
+    flavor: Flavor,
+    oz: i32
+}
+
+// * Use a function to print out the drink flavor and ounces
+fn print_drink(drink: Drink) {
+    // * Use a match expression to print the drink flavor
+    match drink.flavor {
+        Flavor::CocaCola => println!("flavor: Coca Cola"),
+        Flavor::Pepsi    => println!("flavor: Pepsi"),
+    }
+
+    println!("fluid oz: {:?}", drink.oz);
+}
+
+fn main() {
+    let pepsi_drink = Drink {
+        flavor: Flavor::Pepsi,
+        oz: 15,
+    };
+
+    let coca_cola_drink = Drink {
+        flavor: Flavor::CocaCola,
+        oz: 17,
+    };
+
+    print_drink(pepsi_drink);
+    print_drink(coca_cola_drink);
+}
