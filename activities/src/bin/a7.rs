7beac7a915c3b1d9f7087369b0f82117c2c5c150
Santiago Arboleda Londoño <monoprosito@gmail.com>
2021-11-12 19:23:49 -0500

    Print a color according to an Enum

--- activities/src/bin/a7.rs
+++ activities/src/bin/a7.rs
@@ -10,4 +10,31 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+
+// * Use an enum with color names as variants
+enum Color {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Magenta
+}
+
+// * Use a function to print the color name
+// * The function must use the enum as a parameter
+fn print_color(color: Color) {
+    // * Use a match expression to determine which color
+    //   name to print
+    match color {
+        Color::Black   => println!("black!"),
+        Color::Red     => println!("red!"),
+        Color::Green   => println!("green!"),
+        Color::Yellow  => println!("yellow!"),
+        Color::Magenta => println!("magenta!"),
+    }
+}
+
+fn main() {
+    let color = Color::Black;
+    print_color(color);
+}
