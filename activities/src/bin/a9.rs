2db3b0aad4ffddb4dd2a6fc6d6d8c4db6be82da6
Santiago Arboleda Londoño <monoprosito@gmail.com>
2021-11-12 20:33:54 -0500

    Evaluates a tuple of cartesian coordinates

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,24 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+
+// * Use a function that returns a tuple
+fn set_coordinates(x: i32, y: i32) -> (i32, i32) {
+    (x, y)
+}
+
+fn main() {
+    let limit = 5;
+
+    // * Destructure the return value into two variables
+    let (_, y) = set_coordinates(4, 7);
+
+    // * Use an if..else if..else block to determine what to print
+    if y > limit {
+        println!("y-value ({:?}) is greater than the limit ({:?}).", y, limit);
+    } else if y < limit {
+        println!("y-value ({:?}) is less than the limit ({:?}).", y, limit);
+    } else {
+        println!("y-value ({:?}) is equal to the limit ({:?}).", y, limit);
+    }
+}
