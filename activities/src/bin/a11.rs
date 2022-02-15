1a30f1d48fff0b18f5ae94fa9e36ec99b0497489
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-02-14 23:01:47 -0500

    Prints ID and Quantity of a Grocery Item

--- activities/src/bin/a11.rs
+++ activities/src/bin/a11.rs
@@ -9,4 +9,32 @@
 // * Create a function to display the quantity, with the struct as a parameter
 // * Create a function to display the id number, with the struct as a parameter
 
-fn main() {}
+
+// * Use a struct for the grocery item
+struct GroceryItem {
+    id: i32, // Use two i32 fields for the id number
+    quantity: i32 // Use two i32 fields for the quantity
+}
+
+
+// * Create a function to display the id number, with the struct as a parameter
+fn display_item_id(grocery_item: &GroceryItem) {
+    println!("Grocery Item ID: {:?}", grocery_item.id);
+}
+
+
+// * Create a function to display the quantity, with the struct as a parameter
+fn display_item_quantity(grocery_item: &GroceryItem) {
+    println!("Grocery Item Quantity: {:?}", grocery_item.quantity);
+}
+
+
+fn main() {
+    let grocery_item = GroceryItem {
+        id: 1,
+        quantity: 50
+    };
+
+    display_item_id(&grocery_item);
+    display_item_quantity(&grocery_item);
+}
