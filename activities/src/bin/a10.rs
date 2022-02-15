c9eaa4168d2fc082f51385e1cf44188af104b0ac
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-02-14 22:35:20 -0500

    Check if a value is big or small with expressions

--- activities/src/bin/a10.rs
+++ activities/src/bin/a10.rs
@@ -12,4 +12,23 @@
 // * Use a match expression to determine which message
 //   to print
 
-fn main() {}
+
+// * Use a function to print the messages
+fn print_big_or_small(value: i32, limit: i32) {
+    // * Use a boolean variable set to the result of
+    //   an if..else expression to store whether the value
+    //   is > 100 or <= 100
+    let is_big: bool = value > limit;
+
+    // * Use a match expression to determine which message
+    //   to print
+    match is_big {
+        true  => println!("The value is big!"),
+        false => println!("The value is small!"),
+    };
+}
+
+
+fn main() {
+    print_big_or_small(25, 100);
+}
