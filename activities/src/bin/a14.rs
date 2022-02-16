e72aa57c7b2e10983fe07d79a5ba18101c446d2f
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-02-16 00:17:34 -0500

    Print out the name/favorite color of people aged 10 and under

--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -11,4 +11,54 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+
+// Use a struct for a persons age, name, and favorite color
+// The color and name should be stored as a String
+struct Person {
+    name: String,
+    age: i32,
+    favorite_color: String
+}
+
+
+impl Person {
+    fn new(name: String, age: i32, favorite_color: String) -> Self {
+        Self {
+            name: name,
+            age: age,
+            favorite_color: favorite_color
+        }
+    }
+
+    // The name and colors should be printed using a function
+    fn traits(&self) {
+        println!(
+            "{} has {:?} years old, and his/her favorite color is {}.",
+            self.name, self.age, self.favorite_color);
+    }
+}
+
+
+fn main() {
+    let john = Person::new(
+        String::from("John"), 4,
+        String::from("Blue"));
+    let manuela = Person::new(
+        String::from("Manuela"), 7,
+        String::from("Black"));
+    let hugo = Person::new(
+        String::from("Hugo"), 45,
+        String::from("Red"));
+
+    // Create and store at least 3 people in a vector
+    let persons = vec![john, manuela, hugo];
+
+    // Iterate through the vector using a for..in loop
+    for person in persons {
+        // Use an if expression to determine which person's
+        // info should be printed
+        if person.age <= 10 {
+            person.traits();
+        }
+    }
+}
