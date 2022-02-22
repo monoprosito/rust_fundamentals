177e2fd16783141858d1f17b4ef0b52b18aff4bd
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-02-21 21:59:45 -0500

    Checks the school locker of students

--- activities/src/bin/a16.rs
+++ activities/src/bin/a16.rs
@@ -8,7 +8,40 @@
 // * Use a struct containing the student's name and locker assignment
 // * The locker assignment should use an Option<i32>
 
-// * Use a struct containing the student's name and locker assignment
-// * The locker assignment should use an Option<i32>
 
-fn main() {}
+struct Student {
+    name: String,
+    locker: Option<i32>
+}
+
+
+fn main() {
+    let jules = Student {
+        name: String::from("Jules"),
+        locker: None
+    };
+
+    let rue = Student {
+        name: String::from("Rue"),
+        locker: Some(40)
+    };
+
+    let sydney = Student {
+        name: String::from("Sydney"),
+        locker: Some(45)
+    };
+
+    let students: Vec<Student> = vec![
+        jules,
+        rue,
+        sydney
+    ];
+
+    for student in students {
+        match student.locker {
+            Some(locker) => println!("Welcome {}, your assigned school locker is A{:?}.", student.name, locker),
+            None => println!("Welcome {}, you don\'t have a school locker assigned, yet.", student.name)
+        }
+    }
+}
+
