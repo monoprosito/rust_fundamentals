02af4846e3d17f0cc1ed7d79c737a516803d0c9e
Santiago Arboleda Londoño <monoprosito@gmail.com>
2022-02-22 23:54:35 -0500

    Store a furniture stock in a HashMap

--- activities/src/bin/a19.rs
+++ activities/src/bin/a19.rs
@@ -13,4 +13,33 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+
+use std::collections::HashMap;
+
+
+fn main() {
+    // Use a HashMap for the furniture store stock
+    let mut furniture_store_stock: HashMap<String, i8> = HashMap::new();
+
+    // The store has:
+    //   * 5 Chairs
+    //   * 3 Beds
+    //   * 2 Tables
+    //   * 0 Couches
+    furniture_store_stock.insert(String::from("chairs"), 5);
+    furniture_store_stock.insert(String::from("beds"), 3);
+    furniture_store_stock.insert(String::from("tables"), 2);
+    furniture_store_stock.insert(String::from("couches"), 0);
+
+    // Print the name and number of items in stock for a furniture store
+    for (item_name, stock) in furniture_store_stock.iter() {
+        match stock {
+            // If the number of items is 0, print "out of stock" instead of 0
+            0 => println!("Item: {} - Out of stock", item_name),
+            _ => println!("Item: {} - Stock: {:?}", item_name, stock)
+        }
+    }
+
+    // Print the total number of items in stock
+    println!("Total stock: {:?}", furniture_store_stock.values().sum::<i8>());
+}
